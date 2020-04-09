a8233c76b217e2da1f3749b96718154de21e7270
Krunal Patel <Pkrunal616@gmail.com>
2020-04-09 19:38:02 -0400

    add some files and bug bix

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -50,6 +50,8 @@ if __name__ == "__main__":
     print("ID is", id)
     f.close()
 
+    coins_mined = 0
+
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -65,7 +67,8 @@ if __name__ == "__main__":
         # TODO: Get the block from `data` and use it to look for a new proof
         # new_proof = ???
 
-        # block = data["last_block"]
+        block = data["last_block"]
+        new_proof = proof_of_work(block)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -76,4 +79,10 @@ if __name__ == "__main__":
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data["new_block"]:
+            message = "Mining was a success!"
+            coins_mined += 1
+        else:
+            message = "Failed to mine."
+
+        print(f"{message} - Current total coins: {coins_mined}.")
